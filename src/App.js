f8aa0f2b5590ed0e5ea58026d4e2485a5c3a8a84
queuing4oranges <katjazenker@gmail.com>
2022-09-06 22:44:37 +0200

    fetch, input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Quotes from './components/Quotes';
+import NameAge from './components/NameAge';
 
 function App() {
   return (
     <div className="App">
       {/* <div className="quotes__container"><Quotes /></div> */}
-      
+      <NameAge />
 
     </div>
   );
--- src/components/NameAge.js
+++ src/components/NameAge.js
@@ -4,24 +4,49 @@ import React, { useState, useEffect } from 'react';
 export default function NameAge() {
     const [data, setData] = useState(null)
     const [dataLoaded, setDataLoaded] = useState(false)
-
-    const fetchData = async () => {
-        const response = await fetch()
-        const responseData = await response.json()
-        setData(responseData)
-        console.log(data)
-        setData(true)
-    }
-
-    useEffect(() => {
-      fetchData()
-    }, [])
     
+    const getInputValue = (event) => {
+    const agename = event.target.value;
+    console.log(agename)
+    }  
+
+        const url = `https://api.agify.io/?name=${agename}`
+
+        const fetchData = async () => {
+
+        
+
+
+        const response = await fetch()
+        const responseData = await response.json(url)
+        setData(responseData)
+        setData(true)
+}
+
+            
+        // useEffect(() => {
+        // fetchData()
+        // }, [])
+    
+        // console.log(data)
 
 
 
 
   return (
-    <div>NameAge</div>
+    <div>
+        <h2>What's your name according to your age?</h2>
+        <input type="text" onChange={getInputValue} />
+        <button onClick={() =>fetchData}>GO!</button>
+
+        {data && 
+            <div>
+
+            </div>
+        }
+
+
+
+    </div>
   )
 }
