00ff6cdc6848368b9792348c6c81a8ef56fdb2f8
queuing4oranges <katjazenker@gmail.com>
2022-09-08 21:35:57 +0200

    onChange, onClick solved

--- src/components/NameAge.js
+++ src/components/NameAge.js
@@ -2,46 +2,43 @@ import React, { useState, useEffect } from 'react';
 
 
 export default function NameAge() {
+    const [ageName, setAgeName] = useState('')
     const [data, setData] = useState(null)
     const [dataLoaded, setDataLoaded] = useState(false)
     
     const getInputValue = (event) => {
-    const agename = event.target.value;
-    console.log(agename)
+    const name = event.target.value;
+    setAgeName(name)
     }  
+    console.log(ageName)
 
-        const url = `https://api.agify.io/?name=${agename}`
+    const url = `https://api.agify.io/?name=${ageName}`
+    console.log(url)
 
-        const fetchData = async () => {
-
-        
-
-
-        const response = await fetch()
-        const responseData = await response.json(url)
+    const fetchData = async () => {
+        const response = await fetch(url)
+        const responseData = await response.json()
         setData(responseData)
-        setData(true)
-}
-
-            
-        // useEffect(() => {
-        // fetchData()
-        // }, [])
+        setDataLoaded(true)
+        console.log("this is your:" + data)
+    }
     
-        // console.log(data)
+    console.log(data)
 
-
-
-
+    useEffect(() =>{
+      fetchData()
+    },[])
+    
   return (
     <div>
         <h2>What's your name according to your age?</h2>
         <input type="text" onChange={getInputValue} />
-        <button onClick={() =>fetchData}>GO!</button>
+        <button onClick={fetchData}>GO!</button>
 
         {data && 
             <div>
-
+              {data.name}
+              {data.age}
             </div>
         }
 
@@ -50,3 +47,9 @@ export default function NameAge() {
     </div>
   )
 }
+
+/**
+ * https://www.boredapi.com/api/activity
+ * https://api.genderize.io/?name=
+ * https://api.nationalize.io/?name=katja
+ */
