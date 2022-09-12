1b915491692d8c6b01163e1b3fee300a552090f9
queuing4oranges <katjazenker@gmail.com>
2022-09-12 13:58:07 +0200

    new home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,12 @@ export default function Home() {
 
   return (
     <div className="home__container">
-        <h1 className="home__title">How would you like to procrastinate today?</h1>
+        <h1 className="home__title">Let's procrastinate</h1>
         <div className="link__cont">
-        <Link className="home__link" to="/name-age"><p>Check my name.</p></Link>
+        <Link className="home__link" to="/name-age"><p>Check my age.</p></Link>
         <Link className="home__link" to="/quote"><p>Give me a quote.</p></Link>
-        <Link className="home__link" to="/activity"><p>Let's find an activity for me.</p></Link>
-        <Link className="home__link" to="/where"><p>Where am I from.</p></Link>
+        <Link className="home__link" to="/activity"><p>Find an activity.</p></Link>
+        <Link className="home__link" to="/where"><p>Where am I from?</p></Link>
         </div>
     </div>
   )
---
+++ src/components/MaleFemale.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MaleFemale() {
+  return (
+    <div>MaleFemale</div>
+  )
+}
